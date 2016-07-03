eebcb8c6b0569acec42470876b8c90e31d59854b
charles drolet <charles.drokar@gmail.com>
2016-07-03 00:20:38 -0400

    refactoring

--- make.sh
+++ make.sh
@@ -1,69 +1,93 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# Variables
 dotfiles_dir=~/.dotfiles
-excluded_names=(".zzz" ".git" ".." ".")
+ignored_names=(".git" ".." ".")
 
-isExcluded () { 
- 
-    declare -a exlist=( "${excluded_names[@]}" )
-    for element in "${exlist[@]}"; do
-       echo "> $element";
-        if [ "$1" = "$element" ]; then
-            
-            return 0;
+isIgnored() {
+    local ignoredList=( "${ignored_names[@]}" )
+    for element in "${ignoredList[@]}"
+    do
+       if [[ "$1" == "$element" ]]; then
+           echo "Ignoring $1"
+           return 0;
         fi
     done;
     return 1;
 }
 
-makeLinks() {
-  
-  # make a symlink for each file or directory that starts with a . (dot)
+linkFile() {
+    local filename=$(basename "$1")
+    isIgnored "$filename"
+    ignored=$?
+    if [[ "$filename" == .* ]] && [[ "$ignored" -eq 1 ]]
+    then
+      ln -svf $1
+    fi
+}
+
+makeAllLinks() {
   shopt -s dotglob
 
-  for file in $dotfiles_dir/*; do
-    echo "looking at $file"
-    filename=$(basename "$file")
-    isExcluded "$filename"
-    excluded=$?
-    echo "---> $filename, exclude: $excluded"
-    if [[ "$filename" == .* ]] && [[ "$excluded" -eq 1 ]]; then
-      ln -svf $file
-      echo ""
+  for file in $dotfiles_dir/*
+  do
+    linkFile "$file"
+    if [ -d "$file" ] && [ "$ignored" -eq 1 ] 
+    then
+      for file in $file/*
+      do
+        linkFile "$file"
+      done;
     fi
   done
 
-  echo ""
-  echo "CAVEATS"
-  echo "Vim:  If remote server, rm .vimrc.bundles"
-  echo "Bash: If local server, rm .bashrc.local"
-  echo ""
-  echo "Finished."
 }
 
-# Update dotfiles to master branch
-echo
-echo "Updating $dotfiles_dir to master"
-cd $dotfiles_dir;
-git pull origin master;
-cd;
+updateFilesFromRepo() {
+  echo ""
+  echo "Updating $dotfiles_dir to master"
+  echo ""
+  cd $dotfiles_dir
+  git pull origin master
+  cd
+}
+
+addSymbolicLinks() {
+  echo ""
+  echo "Adding symbolics links to home"
+  echo ""
+  if [ "$1" == "--force" -o "$1" == "-f" ]
+  then
+    makeAllLinks
+  else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      makeAllLinks
+    fi
+  fi
+}
+
+cleanup() {
+  unset -f isIgnored 
+  unset -f linkFile
+  unset -f makeAllLinks
+  unset -f updateFilesFromRepo
+  unset -f addSymbolicLinks
+  unset excluded_names
+  unset excluded_names
+}
+
+updateFilesFromRepo
+
+addSymbolicLinks "$1"
+
+cleanup
 
 echo ""
-
-# Make symbolic links
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  makeLinks;
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo ""
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    makeLinks;
-  fi;
-fi;
-
-# Cleanup
-unset -f makeLinks;
-unset -f isExcluded;
-unset excluded_names;
+echo "CAVEATS"
+echo "Vim:  If remote server, rm .vimrc.bundles"
+echo "Bash: If local server, rm .bashrc.local"
+echo ""
+echo "Finished."
 
