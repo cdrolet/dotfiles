41df18e1c6246cb7e1d59beb79688c053ae40403
charles drolet <charles.drokar@gmail.com>
2016-07-10 10:16:20 -0400

    refactoring code + cleanup

--- make.sh
+++ make.sh
@@ -1,51 +1,86 @@
 #!/usr/bin/env bash
 
+out() {
+  printf "\n"
+  for text in "$@"
+  do
+    printf "$text\n"
+  done
+}
+
+indent() {
+  printf "    "
+  $@
+}
+
 initGlobalVariables() {
   shopt -s dotglob
   
-  local source="${BASH_SOURCE[0]}"
-  # resolve $source until the file is no longer a symlink
-  while [ -h "$source" ]; do
-    SOURCE_DIR="$( cd -P "$( dirname "$source" )" && pwd )"
-    source="$(readlink "$source")"
-    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $source != /* ]] && source="$SOURCE_DIR/$source"
-  done
-
-  SOURCE_DIR="$( cd -P "$( dirname "$source" )" && pwd )"
+  SOURCE_DIR=$(getSourceDir)
   BACKUP_DIR="$SOURCE_DIR"/backup
   REVERT_FILE="$SOURCE_DIR"/revert_dotfiles.sh
   TEMP_FILE="$SOURCE_DIR"/files.tmp
   SELECTED_FILES=()
   readarray -t IGNORED_FILES < "$SOURCE_DIR"/.dotignore
   IGNORED_FILES+=("$(basename "$BACKUP_DIR")")
+  MAX_SCAN_LEVEL=1
+}
+
+getSourceDir() {
+  local source="${BASH_SOURCE[0]}"
+  local current
+  # resolve $source until the file is no longer a symlink
+  while [ -h "$source" ]; do
+    current="$( cd -P "$( dirname "$source" )" && pwd )"
+    source="$(readlink "$source")"
+    # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    [[ $source != /* ]] && source="$current/$source"
+  done
+  echo "$( cd -P "$( dirname "$source" )" && pwd )"
+}
+
+updateFromRepo() {
+  out "Updating $SOURCE_DIR to master." ""
+
+  cd "$SOURCE_DIR"
+  git pull origin master
+  git submodule foreach git pull origin master
+  cd
+  out
+}
+
+selectFiles() {
+  out "Scanning dot files:" ""
+  selectDirFiles "$SOURCE_DIR"
 }
 
 selectDirFiles() {
+  local level=0 
+  if [ ! -z "$2" ] 
+  then
+    level=$2
+  fi  
+  
   for file in $1/*
   do
-    
-echo ">>> "$1""
-    
-	if isFileIllegible "$file"
+    if isIllegible "$file"
 	then
       SELECTED_FILES+=("$file")
-      indent "echo "+ Adding $(basename "$file")""
+      indent "echo "+ Selecting $(basename "$file")""
 	else
 	  # look for dotfiles in subfolders
-      if [ -d "$file" ] 
+      if [ -d "$file" ] && [ $level -lt $MAX_SCAN_LEVEL ]
       then
-		if ! isIgnored $(basename "$file") 
-		then
-   	      indent "echo "looking into $file""
-	      selectDirFiles "$file"
-		fi
-	  fi	
+        if ! isIgnored $(basename "$file") 
+        then
+            selectDirFiles "$file" $((level+1))
+        fi
+	  fi
 	fi
   done
 }
 
-isFileIllegible() {
+isIllegible() {
   local filename=$(basename "$1")
   if [[ "$filename" != .* ]] 
   then
@@ -62,11 +97,18 @@ isFileIllegible() {
   # check if the file is in the ignored list
   if isIgnored "$filename"
   then
-    indent "echo - Ignoring "$1""
+    indent "echo - Ignoring "$filename""
     return 1
   fi
 }
 
+areFilesLinked() {
+	if [ -L "$1" ] && [ "$(readlink $1)" = "$SOURCE_DIR/$(basename "$2")" ]
+	then
+	  return 0
+	fi
+	return 1
+}
 
 isIgnored() {
   for element in "${IGNORED_FILES[@]}"
@@ -76,6 +118,42 @@ isIgnored() {
     fi
   done;
   return 1;
+}
+
+
+createSymLinks() {
+  if [ "${#SELECTED_FILES[@]}" -eq 0 ]
+  then
+    out "No new symbolic links required."
+  else
+    confirmLinkCreation "$1"
+  fi
+}
+
+
+confirmLinkCreation() {
+  out "Symbolic links creation required in home." "" \
+      "Overwritten files in home will be saved in backup directory." \
+      "To revert to the backup files, execute 'sh ~/$(basename "$REVERT_FILE")'."
+
+  if [ "$1" == "--force" -o "$1" == "-f" ]
+  then
+    out
+    backupAndLinkFiles
+  else
+    read -p "Do you want to continue? (y/n) " -n 1;
+    out
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      backupAndLinkFiles
+    fi
+  fi
+  out
+}
+
+backupAndLinkFiles() {
+  backupHome
+  symlinkFiles
 }
 
 backupHome() {
@@ -99,14 +177,14 @@ backupHome() {
 
   if [ "${#files[@]}" -eq 0 ]
   then
-    indent "echo "- No file overwritten""
+    indent "echo "- No backup required""
     return
   fi
   
-  rsyncFiles "${files[@]}"
+  copyFiles "${files[@]}"
 }
 
-rsyncFiles() {
+copyFiles() {
   printf "%s\n" $1 > "$TEMP_FILE"
   rsync -av --files-from="$TEMP_FILE" --out-format='    %n%L' ~ "$BACKUP_DIR"
   rm "$TEMP_FILE"
@@ -122,36 +200,7 @@ symlinkFiles() {
   done
 }
 
-areFilesLinked() {
-    
-     echo "are files linked $1 "$SOURCE_DIR/$(basename "$2")""""
-      
-	 
-	if [ -L "$1" ] && [ "$(readlink $1)" = "$SOURCE_DIR/$(basename "$2")" ]
-	then
-	  return 0
-	fi
-	return 1
-}
-
-updateFromRepo() {
-  out "Updating $SOURCE_DIR to master." ""
-
-  cd "$SOURCE_DIR"
-  git pull origin master
-  git submodule foreach git pull origin master
-  cd
-  out
-}
-
 cleanup() {
-  unset -f isIgnored 
-  unset -f symlinkFiles
-  unset -f makeAllLinks
-  unset -f updateFromRepo
-  unset -f areFilesLinked
-  unset -f confirmLinkCreation
-  unset -f initGlobalVariables
 
   unset SELECTED_FILES
   unset SOURCE_DIR
@@ -159,50 +208,26 @@ cleanup() {
   unset REVERT_FILE
   unset TEMP_FILE
   unset IGNORED_FILES
-
-}
-
-confirmLinkCreation() {
-  out "Symbolic links required." "" \
-      "Overwritten files in home will be saved in backup directory." \
-      "To revert to the backup files, execute 'sh ~/$(basename "$REVERT_FILE")'."
-
-  if [ "$1" == "--force" -o "$1" == "-f" ]
-  then
-    out
-    makeAllLinks
-  else
-    read -p "Do you want to continue? (y/n) " -n 1;
-    out
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-      makeAllLinks
-    fi
-  fi
-  out
-}
-
-selectFiles() {
-  out "Scanning dot files:" ""
-  selectDirFiles "$SOURCE_DIR"
-}
-
-makeAllLinks() {
-  backupHome
-  symlinkFiles
-}
-
-out() {
-  printf "\n"
-  for text in "$@"
-  do
-    printf "$text\n"
-  done
-}
-
-indent() {
-  printf "    "
-  $@
+  unset MAX_SCAN_LEVEL
+  
+  unset -f isIgnored 
+  unset -f isIllegible
+  unset -f areFilesLinked
+  unset -f symlinkFiles
+  unset -f backupAndLinkFiles
+  unset -f updateFromRepo
+  unset -f areFilesLinked
+  unset -f confirmLinkCreation
+  unset -f initGlobalVariables
+  unset -f isIllegible
+  unset -f selectDirFiles
+  unset -f selectFiles
+  unset -f createSymLinks
+  unset -f copyFiles
+  unset -f getSourceDir
+  unset -f cleanup
+  
+  out "Finished." ""
 }
 
 initGlobalVariables
@@ -211,14 +236,6 @@ updateFromRepo
 
 selectFiles
 
-if [ "${#SELECTED_FILES[@]}" -eq 0 ]
-then
-  out "No new symbolic links required."
-else
-  confirmLinkCreation "$1"
-fi
+createSymLinks
 
 cleanup
-
-out "Finished." ""
-
