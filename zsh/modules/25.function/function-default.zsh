08a2c7e4e43c543152b6e8f5106decf1f22f4167
charles drolet <charles.drokar@gmail.com>
2016-09-26 10:27:00 -0400

    new external module

--- zsh/modules/25.function/function-default.zsh
+++ zsh/modules/25.function/function-default.zsh
@@ -13,7 +13,7 @@ printHookFunctions () {
 
 # reloads all functions
 # http://www.zsh.org/mla/users/2002/msg00232.html
-r() {
+reloadFunctions() {
     local f=($ZSH_CONFIG_HOME/functions/*(.))
     unfunction $f:t 2> /dev/null
     autoload -Uz $f:t
--- zsh/modules/40.history/history.zsh
+++ zsh/modules/40.history/history.zsh
@@ -17,6 +17,9 @@ HISTSIZE=12000
 # OPTIONS
 ##############################################################
 
+# Treat the '!' character specially during expansion.
+setopt BANG_HIST
+
 # If this is set, zsh sessions will append their history list to the history file, rather than replace it. Thus, multiple parallel zsh sessions will all have the new entries from their history lists added to the history file, in the order that they were entered. The file will still be periodically re-written to trim it when the number of lines grows 20% beyond the value specified by $SAVEHIST (see also the HIST_SAVE_BY_COPY option).
 setopt INC_APPEND_HISTORY
 
@@ -32,11 +35,20 @@ setopt HIST_IGNORE_SPACE
 # If a new command line being added to the history list duplicates an older one, the older command is removed from the list (even if it is not the previous event).
 setopt HIST_IGNORE_ALL_DUPS
 
+# Do not write a duplicate event to the history file.
+setopt HIST_SAVE_NO_DUPS
+
 # Whenever the user enters a line with history expansion, don’t execute the line directly; instead, perform history expansion and reload the line into the editing buffer.
 setopt HIST_VERIFY
 
 # Remove the history (fc -l) command from the history when invoked.
 setopt HIST_NO_STORE
+
+# Beep when accessing non-existent history.
+setopt HIST_BEEP
+
+# Expire a duplicate event first when trimming history.
+setopt HIST_EXPIRE_DUPS_FIRST
 
 # This option both imports new commands from the history file, and also causes your typed commands to be appended to the history file. The history lines are also output with timestamps 
 setopt SHARE_HISTORY
