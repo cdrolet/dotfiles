55ba6f017ef02651537bf0bb9572223f203948b6
charles drolet <charles.drokar@gmail.com>
2016-10-01 19:51:49 -0400

    prompt 9k

--- zsh/modules/70.prompt/init.zsh
+++ zsh/modules/70.prompt/init.zsh
@@ -1,5 +1,5 @@
 
-#source "${0:h}/external/powerlevel9k.zsh-theme" || return 1
+source "${0:h}/external/powerlevel9k.zsh-theme" || return 1
 
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 # https://github.com/bhilburn/powerlevel9k#customizing-prompt-segments
@@ -7,7 +7,7 @@ POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir nvm vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
 # colorcode test
-# for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f"
+#for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f"
 POWERLEVEL9K_NVM_FOREGROUND='000'
 POWERLEVEL9K_NVM_BACKGROUND='072'
 POWERLEVEL9K_SHOW_CHANGESET=true
