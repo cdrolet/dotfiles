556eeb2d2e6723d9f305aaf2d8e58a5f1ecf17d7
charles drolet <charles.drokar@gmail.com>
2016-11-18 17:24:14 -0500

    darwin fixes

---
+++ zsh/modules/80.os/darwin.zsh
@@ -0,0 +1,34 @@
+
+
+
+
+# Darwin ls command does not support --color option.
+alias ls=' ls'
+#alias myls=' ls'
+# use gnu ls instead of bsd ls
+alias myls=' gls -C -F -h --color=always'
+
+# The OSX way for ls colors.
+export CLICOLOR=1
+export LSCOLORS="gxfxcxdxbxegedabagacad"
+
+# brew install zsh-completions
+export FPATH=/usr/local/share/zsh-completions:/usr/local/share/zsh/functions:$FPATH
+
+# activate gls colors
+export ZSH_DIRCOLORS="$ZSH_CONFIG/dircolors-solarized/dircolors.256dark"
+if [[ -a $ZSH_DIRCOLORS ]]; then
+    if [[ "$TERM" == *256* ]]; then
+        which gdircolors > /dev/null && eval "`gdircolors -b $ZSH_DIRCOLORS`"
+    else
+        # standard colors for non-256-color terms
+        which gdircolors > /dev/null && eval "`gdircolors -b`"
+    fi
+else
+    which gdircolors > /dev/null && eval "`gdircolors -b`"
+fi
+
+# start a webcam for screencast
+function webcam () {
+    mplayer -cache 128 -tv driver=v4l2:width=350:height=350 -vo xv tv:// -noborder -geometry "+1340+445" -ontop -quiet 2>/dev/null >/dev/null
+}
