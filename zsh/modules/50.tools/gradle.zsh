3b067eafb43693dbc8fdbc9e545a40b3e0ef87c3
cdrolet <cdrolet@expedia.com>
2021-10-30 18:05:57 -0400

    work config

---
+++ zsh/modules/50.tools/gradle.zsh
@@ -0,0 +1,7 @@
+alias grad="echoAndRun ./gradlew"
+alias gradc="echoAndRun ./gradlew clean"
+alias gradcb="echoAndRun ./gradlew clean build"
+alias gradt="echoAndRun ./gradlew check"
+alias gradr="echoAndRun ./gradlew run"
+alias gradb="echoAndRun ./gradlew build"
+alias gradbp="echoAndRun ./gradlew build --scan"
